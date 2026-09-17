443df0fa86ac5b38486029cdcc7c8715f7e231d7
gitomwtyczka <tomasz.brzozowski@impresjapr.pl>
2026-09-17 10:04:25 +0200

    feat: init biblia-worker config.py [media-dev-impl-01]

---
+++ agents/biblia-worker/config.py
@@ -0,0 +1,22 @@
+"""
+biblia-worker / config.py
+Zweryfikowane parametry produkcyjne (sesja 16.09.2026, 5/5 sukces)
+"""
+
+VSE_BASE    = "https://vse.impresjapr.pl"
+PORTAL_ID   = "2b047d7d-15a1-4d2f-8463-f89c2275bb73"
+USER_ID     = "4b97ab0c-98ee-46c6-9be8-d86adc4cb38a"
+LLM         = "claude"
+PUB_TYPE    = "full_analysis"
+
+YT_CHANNEL  = "UCNXh5eIlMVxnUBpTMKUp4CA"          # Prawy TV
+PLAYLIST    = "PLw7UeigJuyWkUzzvhS1vZX0H251raaYa7"
+
+WP_CONTAINER  = "prawy-wordpress"
+VSE_CONTAINER = "vse-api"
+VPS           = "ubuntu@147.224.162.100"
+SSH_KEY       = r"C:\Users\tomas2\.ssh\oracle-crimson.key"
+
+SPREADSHEET_ID   = "1zqwvS784EaZh1EJIcXk1DliAau1r4X15ENFJjloDSaM"
+SHEETS_TAB       = "Biblia"
+SHEETS_GID       = "893663019"
